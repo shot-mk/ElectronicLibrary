b44435129bf1d6b814bf965d6b4bb324b396c68c
nikita <shot-mk@yandex.ru>
2014-07-25 15:22:59 +0300

    adding admin info on Application Startup

--- src/main/java/com/shotmk/el/config/WebAppConfig.java
+++ src/main/java/com/shotmk/el/config/WebAppConfig.java
@@ -56,5 +56,4 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         return converter;
     }
 
-
 }
--- src/main/java/com/shotmk/el/web/MainController.java
+++ src/main/java/com/shotmk/el/web/MainController.java
@@ -1,17 +1,41 @@
 package com.shotmk.el.web;
 
+import com.shotmk.el.entity.User;
+import com.shotmk.el.entity.enums.UserRoleEnum;
+import com.shotmk.el.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.PostConstruct;
+
 @Controller
 @RequestMapping("/")
 public class MainController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
     public String start(Model model) {
         return "index";
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        User user = userService.getUser("admin");
+        if (user == null) {
+            String login = "admin";
+            String password = "admin";
+            String role = UserRoleEnum.ADMIN.name();
+            ShaPasswordEncoder encoder = new ShaPasswordEncoder();
+            String encodedPassword = encoder.encodePassword(password, null);
+            user = new User(login, encodedPassword, role);
+            userService.addUser(user);
+        }
     }
 
 }
